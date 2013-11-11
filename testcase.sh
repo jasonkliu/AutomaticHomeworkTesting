ceb424e7eb88fd79560aafdd60c02427111c3eb8
Jason Liu <sprintround@gmail.com>
2013-11-11 17:41:37 -0500

    revisions to script if then loops

--- testcase.sh
+++ testcase.sh
@@ -4,22 +4,28 @@
 echo "Script made by Jason Liu, November 2013"
 echo -n "Enter name of file (*.scm): "
 read a
-# echo "$a"
 
 # Written for Racket 5.3.6
 direc=~/../../Applications/Racket\ v5.3.6/bin/
 
+# Checks if directory exists
 if [ ! -d "$direc" ]; then
    echo ""
    echo "Directory does not exist ... please check your Applications folder"
    exit
 fi
 
-cp hw6_testcases.scm  ~/../../Applications/Racket\ v5.3.6/bin/
-cp $a  ~/../../Applications/Racket\ v5.3.6/bin/
-cd ~/../../Applications/Racket\ v5.3.6/bin/
+cp hw6_testcases.scm "$direc"
+cp $a "$direc"
+cd "$direc"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Running in: $DIR"
 
-./plt-r5rs hw6_testcases.scm > output.txt
+if [ $a == "hw6.scm" ]
+then
+   ./plt-r5rs hw6_testcases.scm > output.txt
+else
+   echo ""
+   echo "File not supported."
+fi
