bce1a999d9697940a3f776040b743091de3a6274
Jason Liu <sprintround@gmail.com>
2013-11-13 11:10:35 -0500

    rewrote testcases as functions, moved to template doc

--- hw6_testcases.scm
+++ hw6_testcases.scm
@@ -5,68 +5,65 @@
 (load "hw6.scm")
 (display "Test cases for Homework 6") (newline) (newline)
 
-;************************************************************
-(display "------------PROBLEM 1---------------") (newline)
+(define testcase ; begin and newline don't work well with if statements
+  (lambda (problem number function output symbol)
+    (if (equal? number 1) 
+        ; if it's the first problem in the series
+        (firsttest problem number function output symbol)
+        ; else
+        (elsetest problem number function output symbol))))
 
-; Template for new test case
-(display "----- TEST 1 -----") (newline)
-(display " (ram-read 4 ram-ex1) ") (newline)
-(if (equal? (ram-read 4 ram-ex1) '(0 0 0 0 0 0 0 0 0 0 1 0 1 0 1 0))
-    (begin (display "Your output is correct: ") (display '(0 0 0 0 0 0 0 0 0 0 1 0 1 0 1 0)))
-    (begin (display "Your output is wrong: ")(ram-read 4 ram-ex1)))
-(newline) (newline)
+(define firsttest
+  (lambda (problem number function output symbol)
+    (begin (display "------------PROBLEM ") (display problem) (display "---------------"))
+    (newline)
+    (begin (begin (display "---- TEST ") (display number) (display " -----")))
+    (newline)
+    (display symbol) (newline)
+   ; (display output) (newline)
+    (if (equal? function output)
+        (begin (display "Your output is correct: ") (display function))
+        (begin (display "Your output is wrong: ") function))
+    (newline) (newline)))
 
-;************************************************************
-(display "------------PROBLEM 2---------------") (newline)
+(define elsetest
+  (lambda (problem number function output symbol)
+    (begin (begin (display "---- TEST ") (display number) (display " -----")))
+    (newline)
+    (display symbol) (newline)
+    ; (display output) (newline)
+    (if (equal? function output)
+        (begin (display "Your output is correct: ") (display function))
+        (begin (display "Your output is wrong: ") function))
+    (newline) (newline)))
 
-(display "----- TEST 1 -----") (newline)
-(display " (bits->int '(1 1 1 1 0 1 0 1 0 0 0 1)) ") (newline)
-(if (equal? (bits->int '(1 1 1 1 0 1 0 1 0 0 0 1)) '3921)
-    (begin (display "Your output is correct: ") (display '3921))
-    (begin (display "Your output is wrong: ")(bits->int '(1 1 1 1 0 1 0 1 0 0 0 1)))) 
-(newline) (newline)
+;;************************************************************
+(testcase 1 1 (ram-read 4 ram-ex1) 
+          '(0 0 0 0 0 0 0 0 0 0 1 0 1 0 1 0) '(ram-read 4 ram-ex1))
 
-(display "----- TEST 2 -----") (newline)
-(display " (int->bits 583) ") (newline)
-(if (equal? (int->bits 583) '(1 0 0 1 0 0 0 1 1 1))
-    (begin (display "Your output is correct: ") (display '(1 0 0 1 0 0 0 1 1 1)))
-    (begin (display "Your output is wrong: ")(int->bits 583)))
-(newline) (newline)
+;;************************************************************
+(testcase 2 1 (bits->int '(1 1 1 1 0 1 0 1 0 0 0 1)) 
+          '3921 '(bits->int '(1 1 1 1 0 1 0 1 0 0 0 1)))
 
-;************************************************************
-(display "------------PROBLEM 3---------------") (newline)
+(testcase 2 2 (int->bits 583) 
+          '(1 0 0 1 0 0 0 1 1 1) '(bits->int '(int->bits 583)))
 
-(display "----- TEST 1 -----") (newline)
-(display " (incr-pc 5 config2) ") (newline)
-(if (equal? (cadr (config-cpu (incr-pc 5 config2))) '(pc (0 0 0 0 0 0 0 0 1 1 0 1)))
-    (begin (display "Your output is correct: ") (display '(pc (0 0 0 0 0 0 0 0 1 1 0 1))))
-    (begin (display "Your output is wrong: ")(cadr (config-cpu (incr-pc 5 config2)))))
-(newline) (newline)
+;;************************************************************
+(testcase 3 1 (cadr (config-cpu (incr-pc 5 config2))) 
+          '(pc (0 0 0 0 0 0 0 0 1 1 0 1)) '(cadr (config-cpu (incr-pc 5 config2))))
 
-(display "----- TEST 2 -----") (newline)
-(display " (incr-pc 5000 config2) ") (newline)
-(if (equal? (cadr (config-cpu (incr-pc 5000 config2))) '(pc (0 0 1 1 1 0 0 1 0 0 0 0)))
-    (begin (display "Your output is correct: ") (display '(pc (0 0 1 1 1 0 0 1 0 0 0 0))))
-    (begin (display "Your output is wrong: ")(cadr (config-cpu (incr-pc 5 config2)))))
-(newline) (newline)
+(testcase 3 2 (cadr (config-cpu (incr-pc 5000 config2))) 
+          '(pc (0 0 1 1 1 0 0 1 0 0 0 0)) '(cadr (config-cpu (incr-pc 5000 config2))))
 
-;************************************************************
-(display "------------PROBLEM 4---------------") (newline)
+;;************************************************************
+(testcase 4 1 (config-cpu (add 3 config3)) 
+          '((acc (0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1)) (pc (0 0 0 0 0 0 0 0 0 1 1 1)) (run-flag (1)) (aeb (0))) '(config-cpu (add 3 config3)))
 
-; not sure if these are correct
+(testcase 4 2 (config-cpu (sub 4 config4)) 
+          '((acc (0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0)) (pc (0 0 0 0 0 0 0 0 0 0 0 0)) (run-flag (1)) (aeb (0))) '(config-cpu (sub 4 config4)))
 
-(display "----- TEST 1 -----") (newline)
-(display " (config-cpu (add 3 config3) ") (newline)
-(if (equal? (config-cpu (add 3 config3)) '((acc (0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1)) (pc (0 0 0 0 0 0 0 0 0 1 1 1)) (run-flag (1)) (aeb (0))))
-    (begin (display "Your output is correct: ") (display '((acc (0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1)) (pc (0 0 0 0 0 0 0 0 0 1 1 1)) (run-flag (1)) (aeb (0)))))
-    (begin (display "Your output is wrong: ") (config-cpu (add 3 config3))))
-(newline) (newline)
 
-(display "----- TEST 2 -----") (newline)
-(display " (config-cpu (sub 4 config4)) ") (newline)
-(if (equal? (config-cpu (sub 4 config4)) '((acc (0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0)) (pc (0 0 0 0 0 0 0 0 0 0 0 0)) (run-flag (1)) (aeb (0))))
-    (begin (display "Your output is correct: ") (display '((acc (0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0)) (pc (0 0 0 0 0 0 0 0 0 0 0 0)) (run-flag (1)) (aeb (0)))))
-    (begin (display "Your output is wrong: ") (config-cpu (sub 4 config4))))
-(newline) (newline)
+
+
 
 (display 'done)
