56b698637011645b0bcf78c522419d0c0c8aaf0e
Jason Liu <sprintround@gmail.com>
2013-11-17 13:40:59 -0500

    testcase support for hw7

--- testcase.sh
+++ testcase.sh
@@ -13,26 +13,44 @@ direc=~/../../Applications/Racket\ v5.3.6/bin/
 # Checks if directory exists
 if [ ! -d "$direc" ]; then
    echo ""
-   echo "Directory does not exist ... please check your Applications folder"
+   echo "Directory does not exist ... please check your applications folder"
    exit
 fi
 
-cp hw6_testcases.scm "$direc"
-cp $a "$direc"
-cd "$direc"
+# Copying files
+if [ $a == "hw6.scm" ]
+then
+   cp hw6/hw6_testcases.scm "$direc"
+   cp $a "$direc"
+   cd "$direc"
+elif [ $a == "hw7.scm" ]
+then
+   cp hw7/hw7_testcases.scm "$direc"
+   cp $a "$direc"
+   cd "$direc"
+else
+   echo ""
+   echo "File not supported."
+   exit
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Running in: $DIR"
 
+# Running files
 if [ $a == "hw6.scm" ]
 then
    ./plt-r5rs hw6_testcases.scm > output.txt
+elif [ $a == "hw7.scm" ]
+then
+   ./plt-r5rs hw7_testcases.scm > output.txt
 else
    echo ""
    echo "File not supported."
+   exit
 fi
 
 cp output.txt "$orig"
 
-# Remove files
-# rm output.txt; rm hw6_testcases.scm; rm hw6.scm
+echo ""
+echo "done"
