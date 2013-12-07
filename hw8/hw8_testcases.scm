8ed935c421d4d26a4f83cd2a6b06c760749d13a2
Jason Liu <sprintround@gmail.com>
2013-12-07 15:57:02 -0500

    two random tictactoe cases

--- hw8/hw8_testcases.scm
+++ hw8/hw8_testcases.scm
@@ -54,11 +54,57 @@
 (testcase 1 12 (g1 'play 'x 3 2) 'x-won '(g1 'play 'x 3 2))
 (testcase 1 13 (g1 'play 'o 2 3) 'game-over '(g1 'play 'o 2 3))
 
+(display "---------------------NEXT TEST CASE---------------")(newline)(newline)
+
 (define g2 (make-ttt-game)) 
 (testcase 1 14 (g2 'play 'o 2 3) '((- - -) (- - o) (- - -)) '(g2 'play 'o 2 3))
 (testcase 1 15 (g1 'board) '((- x o) (- x -) (- x o)) '(g1 'board))
 (testcase 1 16 (g1 'reset) '((- - -) (- - -) (- - -)) '(g1 'reset))
 (testcase 1 17 (g2 'board) '((- - -) (- - o) (- - -)) ' (g2 'board))
+
+(display "---------------------NEXT TEST CASE---------------")(newline)(newline)
+
+(define g6 (make-ttt-game))
+(testcase 1 20 (g6 'play 'o 3 1) '((- - -) (- - -) (o - -)) '(g6 'play 'o 3 1)) 
+(testcase 1 21 (g6 'play 'x 3 2) '((- - -) (- - -) (o x -)) '(g6 'play 'x 3 2)) 
+(testcase 1 22 (g6 'play 'x 3 1) 'out-of-turn '(g6 'play 'x 3 1)) 
+(testcase 1 23 (g6 'play 'x 3 3) 'out-of-turn '(g6 'play 'x 3 3)) 
+(testcase 1 24 (g6 'play 'x 3 1) 'out-of-turn '(g6 'play 'x 3 1)) 
+(testcase 1 25 (g6 'play 'o 2 2) '((- - -) (- o -) (o x -)) '(g6 'play 'o 2 2)) 
+(testcase 1 26 (g6 'play 'x 1 2) '((- x -) (- o -) (o x -)) '(g6 'play 'x 1 2)) 
+(testcase 1 27 (g6 'play 'o 3 2) 'square-filled '(g6 'play 'o 3 2)) 
+(testcase 1 28 (g6 'play 'x 1 3) 'out-of-turn '(g6 'play 'x 1 3)) 
+(testcase 1 29 (g6 'play 'o 1 1) '((o x -) (- o -) (o x -)) '(g6 'play 'o 1 1)) 
+(testcase 1 30 (g6 'play 'o 3 2) 'out-of-turn '(g6 'play 'o 3 2)) 
+(testcase 1 31 (g6 'play 'o 3 1) 'out-of-turn '(g6 'play 'o 3 1)) 
+(testcase 1 32 (g6 'play 'x 2 3) '((o x -) (- o x) (o x -)) '(g6 'play 'x 2 3)) 
+(testcase 1 33 (g6 'play 'o 2 1) 'o-won '(g6 'play 'o 2 1))
+
+(display "---------------------NEXT TEST CASE---------------")(newline)(newline)
+
+(define g9 (make-ttt-game))
+(testcase 1 40 (g9 'play 'o 3 1) '((- - -) (- - -) (o - -)) '(g9 'play 'o 3 1)) 
+(testcase 1 41 (g9 'play 'x 3 2) '((- - -) (- - -) (o x -)) '(g9 'play 'x 3 2)) 
+(testcase 1 42 (g9 'play 'x 3 1) 'out-of-turn '(g9 'play 'x 3 1)) 
+(testcase 1 43 (g9 'play 'x 3 3) 'out-of-turn '(g9 'play 'x 3 3)) 
+(testcase 1 44 (g9 'play 'x 3 1) 'out-of-turn '(g9 'play 'x 3 1)) 
+(testcase 1 45 (g9 'play 'o 2 2) '((- - -) (- o -) (o x -)) '(g9 'play 'o 2 2)) 
+(testcase 1 46 (g9 'play 'x 1 2) '((- x -) (- o -) (o x -)) '(g9 'play 'x 1 2)) 
+(testcase 1 47 (g9 'play 'o 3 2) 'square-filled '(g9 'play 'o 3 2)) 
+(testcase 1 48 (g9 'play 'x 1 3) 'out-of-turn '(g9 'play 'x 1 3)) 
+(testcase 1 49 (g9 'play 'o 1 1) '((o x -) (- o -) (o x -)) '(g9 'play 'o 1 1)) 
+(testcase 1 50 (g9 'play 'o 3 2) 'out-of-turn '(g9 'play 'o 3 2)) 
+(testcase 1 51 (g9 'play 'o 3 1) 'out-of-turn '(g9 'play 'o 3 1)) 
+(testcase 1 52 (g9 'play 'o 2 1) 'out-of-turn '(g9 'play 'o 2 1)) 
+(testcase 1 53 (g9 'play 'o 3 3) 'out-of-turn '(g9 'play 'o 3 3)) 
+(testcase 1 54 (g9 'play 'o 3 1) 'out-of-turn '(g9 'play 'o 3 1)) 
+(testcase 1 55 (g9 'play 'x 1 3) '((o x x) (- o -) (o x -)) '(g9 'play 'x 1 3)) 
+(testcase 1 56 (g9 'play 'x 2 3) 'out-of-turn '(g9 'play 'x 2 3)) 
+(testcase 1 57 (g9 'play 'o 3 2) 'square-filled '(g9 'play 'o 3 2)) 
+(testcase 1 58 (g9 'play 'o 3 1) 'square-filled '(g9 'play 'o 3 1)) 
+(testcase 1 59 (g9 'play 'x 3 2) 'out-of-turn '(g9 'play 'x 3 2)) 
+(testcase 1 60 (g9 'play 'o 3 3) 'o-won '(g9 'play 'o 3 3)) 
+
 
 ;;************************************************************
 
