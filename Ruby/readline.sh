b71fba11cacf8381c337835cfb08d99d999ab76c
Jason Liu <jason@jasonkliu.com>
2014-03-08 16:53:39 -0500

    More robust function over80 in readline.sh

--- Ruby/readline.sh
+++ Ruby/readline.sh
@@ -22,14 +22,15 @@ else
    done
 fi
 
-# Additional Tests
-
 # Check for lines > 80 characters
-grep -n '^.\{80\}' testfile > over.txt
-if [ -s over.txt ]; then # if over.txt exists && size>0
-   echo "Warning: Lines over 80 characters in file"
-else  
-   echo "Passed Test: No lines over 80 characters"
-fi
-
-
+function over80
+{
+ls -1p > filelist # make a file list with each on one line
+while read -r line ; do # Read lines from stdin
+   grep -n '^.\{80\}' "$line" > over.txt # TODO: Hide stderr if >1
+   if [ -s over.txt ]; then # if over.txt exists && size>0
+      echo "Warning: Lines over 80 characters in file '$line'"
+   fi
+done < filelist
+rm over.txt filelist
+}
