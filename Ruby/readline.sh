8e90db0137e719089ece88a7d64dd6bb22aaa612
Jason Liu <jason@jasonkliu.com>
2014-03-09 12:42:11 -0700

    Change to folder, check for over80, clean up

--- Ruby/readline.sh
+++ Ruby/readline.sh
@@ -3,6 +3,19 @@
 echo "Script made by Jason Liu, March 2014"
 echo ""
 
+# Check for lines > 80 characters
+function over80 ()      # TODO: Dive deeper into folders
+{
+ls -1p > filelist # make a file list with each on one line
+while read -r line ; do # Read lines from stdin
+   grep -n '^.\{80\}' "$line" > over.txt # TODO: Hide stderr if >1
+   if [ -s over.txt ]; then # if over.txt exists && size>0
+      echo "Warning: Lines over 80 characters in file '$line'"
+   fi
+done < filelist
+rm over.txt filelist
+}
+
 git --version 2>&1 >/dev/null # find git --version
 GIT_IS_AVAILABLE=$?           # should be 0 if git is installed
 if [ $GIT_IS_AVAILABLE -ne 0 ]; then 
@@ -34,31 +47,29 @@ if [ -t 0 ]; then # if argc = 1 (no stdin)
    current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
    echo "Running in: $current"
    while read -r line ; do # Read each line in $a
+	  # Thanks to https://bbs.archlinux.org/viewtopic.php?id=111189
+	  folder=`echo "${line##*/}/"`  # Find directory of cloning
       line=`echo -e $line | sed -e "s/https/git/"`
       line=`echo -e $line | sed -e "s/$/.git/"`
 	  echo -e "$line" 
-	  #git clone $line
+	  git clone $line; echo ""
+	  cd $folder                      # Change to folder directory
+	  over80                          # Check if over 80 lines in file
+      cd ../; rm -rf $folder; echo "" # Clean up contents
    done < $a
 
 # Quick mode: read from stdin and run automatically
 else
    while read -r line ; do # Read lines from stdin
+	  # Thanks to https://bbs.archlinux.org/viewtopic.php?id=111189
+	  folder=`echo "${line##*/}/"`  # Find directory of cloning
       line=`echo -e $line | sed -e "s/https/git/"`
       line=`echo -e $line | sed -e "s/$/.git/"`
       echo -e "$line" 
-	  #git clone $line
+	  git clone $line; echo ""
+	  cd $folder                      # Change to folder directory
+	  over80                          # Check if over 80 lines in file
+      cd ../; rm -rf $folder; echo "" # Clean up contents
    done
 fi
 
-# Check for lines > 80 characters
-function over80
-{
-ls -1p > filelist # make a file list with each on one line
-while read -r line ; do # Read lines from stdin
-   grep -n '^.\{80\}' "$line" > over.txt # TODO: Hide stderr if >1
-   if [ -s over.txt ]; then # if over.txt exists && size>0
-      echo "Warning: Lines over 80 characters in file '$line'"
-   fi
-done < filelist
-rm over.txt filelist
-}
